a9ea031885dbc5ecbbf4655be1650c2448b31a18
j-wass <jonahwasserman97@gmail.com>
2022-10-29 20:54:55 -0700

    add coverage implementation

--- liqui/coverage.go
+++ liqui/coverage.go
@@ -1,8 +1,38 @@
 package liqui
 
-import "golang.org/x/net/html"
+import (
+	utils "diesel/utils"
+	"fmt"
+	"strings"
 
-func Coverage(liquipediaHTML *html.Node) string {
-	// todo implement
-	return ""
+	"golang.org/x/net/html"
+)
+
+func Coverage(liquipediaHTML *html.Node, liquiURL string) string {
+	coverageItems := make([]string, 0)
+	coverageItems = append(coverageItems, fmt.Sprintf("[**Liquipedia**](%s)", liquiURL)) 
+
+
+	octaneGGLink := utils.Query(liquipediaHTML, "a[href^=\"https://octane.gg/events\"]")
+	octaneGGHref := "#"
+	if octaneGGLink != nil{
+		octaneGGHref = utils.AttrOr(octaneGGLink, "href", "")
+		if octaneGGHref != ""{
+			coverageItems = append(coverageItems, fmt.Sprintf("[**Octane.gg**](%s)", octaneGGHref))
+		}
+	}
+	
+	startGGLink := utils.Query(liquipediaHTML, "a[href^=\"https://www.start.gg/tournament\"]")
+	startGGHref := "#"
+	if startGGLink != nil{
+		startGGHref = utils.AttrOr(startGGLink, "href", "")
+		if startGGHref != ""{
+			coverageItems = append(coverageItems, fmt.Sprintf("[**Start.gg**](%s)", startGGHref))
+		}
+	}
+	
+	markdown := "# Coverage\n\n"
+	
+	markdown += strings.Join(coverageItems, " **/ /** ")
+	return markdown
 }
