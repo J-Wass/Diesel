1e36e29c050be11f95178fa1ff7eea69db4f4afd
j-wass <jonahwasserman97@gmail.com>
2023-03-22 23:40:31 -0400

    fix old schedules and prizepool parenthesis

--- liqui/schedule.go
+++ liqui/schedule.go
@@ -20,9 +20,12 @@ func Schedule(liquipediaHTML *html.Node, dayNumber int) string {
 		hour, minute, _ := day[0].Clock()
 		dayOfWeek := day[0].Weekday().String()
 		dayMarkdown := fmt.Sprintf("\n|Day %d|%s|[**%d:%02d**](https://www.google.com/search?q=%d:%02d+GMT)|", i+1, dayOfWeek, hour, minute, hour, minute)
+		if i < dayNumber - 1{
+			dayMarkdown = fmt.Sprintf("\n|~~Day %d~~|~~%s~~|~~[**%d:%02d**](https://www.google.com/search?q=%d:%02d+GMT)~~|", i+1, dayOfWeek, hour, minute, hour, minute)
+		}
 		if i == dayNumber-1{
 			dayMarkdown += "**Today**|"
-		}else{
+		} else{
 			dayMarkdown += "|"
 		}
 		markdownStringBuilder.WriteString(dayMarkdown)
